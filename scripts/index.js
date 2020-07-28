0eb74d859e21d322ea5813d8f4fd340d55bc96a6
Nizam <nizam@edyst.com>
2020-07-28 15:10:07 +0530

    transition added

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,12 @@
-alert("Site under construction");
+function parallax(e){
+    this.querySelectorAll('.layer').forEach(layer => {
+        const speed = layer.getAttribute('data-speed'); 
+
+        const x = (window.innerWidth - e.pageX*speed)/100
+        const y = (window.innerHeight - e.pageY*speed)/100;
+    
+        layer.style.transform = `translateX(${x}px) translateY(${y}px)`;
+    });
+}
+
+document.addEventListener("mousemove", parallax);
